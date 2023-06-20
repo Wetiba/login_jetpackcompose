a026a3340022818f28add384a2e95bab44dbbd37
wetiba <erickw006@gmail.com>
2023-06-20 17:45:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/composer/screen/ScreenMain.kt
@@ -0,0 +1,20 @@
+package com.example.composer.screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.composer.Routes
+
+@Composable
+fun ScreenMain(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routes.Login.route) {
+
+        composable(Routes.Login.route) {
+            LoginPage(navController = navController)
+        }
+    }
+}
